7ece16cde63b5b90c5fd1347de269e1cb428c3aa
brtgio <67073086+brtgio@users.noreply.github.com>
2024-03-21 09:16:59 -0700

    Update and rename src/project.v to tt_um_top.v

--- tt_um_top.v
+++ tt_um_top.v
@@ -16,9 +16,18 @@ module tt_um_example (
     input  wire       rst_n     // reset_n - low to reset
 );
 
+    top (
+        .clk(clk), 
+        .reset_n(rst_n),
+        .save_a_n(uio_in[5]),
+        .save_b_n(uio_in[6]),
+        .data_input(uio_in[4:0]),
+        .uart_txd(uio_out[1]),
+        .uart_tx_busy(uio_out[2])
+    );
   // All output pins must be assigned. If not used, assign to 0.
-  assign uo_out  = ui_in + uio_in;  // Example: ou_out is the sum of ui_in and uio_in
-  assign uio_out = 0;
-  assign uio_oe  = 0;
+    assign uio_out [7:3] = 5'b00000;
+    assign uo_out [7:0] = 8'b00000000;
+    assign uio_eo [7:0] = 8'b00000000;;
 
 endmodule
