df097970d1f88a10988d40b6f762a89e033c5009
brtgio <67073086+brtgio@users.noreply.github.com>
2024-03-24 13:01:15 -0700

    Update tt_um_top.v

--- src/tt_um_top.v
+++ src/tt_um_top.v
@@ -31,7 +31,7 @@ module tt_um_top (
     // Assigning the non-used output bits to 0
     assign uo_out [7:0] = 8'b00000000;
     assign uio_out [4:0] = 5'b00000;
-    assign uio_out [7] = 1b'0;
+    assign uio_out [7] = 1'b0;
     // Configuring uio_oe as output (assigning all bits to 1)
     assign uio_oe[7:0] = 8'b11100000;
 
