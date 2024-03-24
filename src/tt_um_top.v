31f405d47326c47ef20a21866cdc9a7e67035344
brtgio <67073086+brtgio@users.noreply.github.com>
2024-03-23 21:26:56 -0700

    Update tt_um_top.v

--- src/tt_um_top.v
+++ src/tt_um_top.v
@@ -10,18 +10,18 @@ module tt_um_top (
     input  wire [7:0] uio_in,   // IOs: Input path
     output wire [7:0] uio_out,  // IOs: Output path
     output wire [7:0] uio_oe,   // IOs: Enable path (active high: 0=input, 1=output)
-    input  wire       enable,   // enable signal
-    input  wire       clock,    // clock signal
-    input  wire       reset     // reset signal
+    input  wire       ena,      // will go high when the design is enabled
+    input  wire       clk,      // clock
+    input  wire       rst_n     // reset_n - low to reset
 );
 
-    uart_tx uart_inst (
-        .clk(clock),                // Connect top-level clock signal
-        .resetn(~reset),            // Connect top-level reset signal (inverted)
-        .uart_tx_en(enable),       // Connect enable signal to UART transmitter
-        .uart_tx_data(ui_in[6:3]), // Connect data_input signal to UART transmitter
-        .uart_txd(uio_out[0]),     // UART transmit data connected to the least significant bit of uio_out
-        .uart_tx_busy(uio_out[7])  // UART transmit busy signal connected to the most significant bit of uio_out
+    toptop  top_inst (
+        .clk(clk),                // Connect top-level clock signal
+        .reset_n(rst_n),          // Connect top-level reset signal
+        .uart_tx_en(ena),         // Connect enable signal to UART transmitter
+        .uart_tx_data(ui_in[6:3]),// Connect data_input signal to UART transmitter
+        .uart_txd(uio_out[0]),    // UART transmit data connected to the least significant bit of uio_out
+        .uart_tx_busy(uio_out[7]) // UART transmit busy signal connected to the most significant bit of uio_out
     );
 
     // Assigning the non-used output bits to 0
@@ -31,6 +31,7 @@ module tt_um_top (
     assign uio_oe[7:0] = 8'b11111111;
 
 endmodule
+
 
 
 
