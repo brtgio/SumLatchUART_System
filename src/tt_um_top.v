2cdfdaf8e737b3d7b66aef550e1fdc71aad2581a
brtgio <67073086+brtgio@users.noreply.github.com>
2024-03-23 21:14:46 -0700

    Update tt_um_top.v

--- src/tt_um_top.v
+++ src/tt_um_top.v
@@ -16,17 +16,18 @@ module tt_um_top (
 );
 
     toptop  top_inst (
-        .clk(ui_in [7]),         
-        .reset_n(ui_in [0]),      
-        .save_a_n(ui_in [1]),
-        .save_b_n(ui_in [2]),
-        .data_input(ui_in [6:3]),
-        .uart_txd(uio_out [0]),
-        .uart_tx_busy(uio_out [1])
+        .clk(ui_in [7]),         // Clock connected to the most significant bit of ui_in
+        .reset_n(ui_in [0]),      // Reset signal connected to the least significant bit of ui_in
+        .save_a_n(ui_in [1]),     // Save_a_n signal connected to the second least significant bit of ui_in
+        .save_b_n(ui_in [2]),     // Save_b_n signal connected to the third least significant bit of ui_in
+        .data_input(ui_in [6:3]), // Data_input signal connected to bits [6:3] of ui_in
+        .uart_txd(uio_out [0]),   // UART transmit data connected to the least significant bit of uio_out
+        .uart_tx_busy(uio_out [7]) // UART transmit busy signal connected to the most significant bit of uio_out
     );
-    // Assigning the non-used bits to 0
-    assign uo_out [7:2] = 6'b00000000;
-    assign uio_out [7:2] = 6'b00000000;
+
+    // Assigning the non-used output bits to 0
+    assign uo_out [7:0] = 8'b00000000;
+
     // Configuring uio_oe as output (assigning all bits to 1)
     assign uio_oe [7:0] = 8'b11111111;
 endmodule
