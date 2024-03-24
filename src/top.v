69097be4a19dfb5036dfc7180f5d08fee0cd9a29
brtgio <67073086+brtgio@users.noreply.github.com>
2024-03-24 12:31:09 -0700

    Update top.v

--- src/top.v
+++ src/top.v
@@ -17,30 +17,27 @@ wire [4:0] sum_reg;
 
 // Instanciación del módulo eight_bit_latch
 latch_2x8 latch_inst (
-    .clk(clk),
-    .reset_n(reset_n),
-    .save_a_n(save_a_n),
-    .save_b_n(save_b_n),
-    .data_input(data_input),
-    .q_a(a), // Salida del latch para el primer operando
-    .q_b(b)  // Salida del latch para el segundo operando
+.clk(clk),
+.reset_n(reset_n),
+.save_a_n(save_a_n),
+.save_b_n(save_b_n),
+.data_input(data_input),
+.q_a(a), // Salida del latch para el primer operando
+.q_b(b)  // Salida del latch para el segundo operando
 );
-
 // Instanciación del módulo sum4bit_reg
 fourbit_adder sum_inst (
-    .a(a),
-    .b(b),
-    .sum(sum_reg)
+.a(a),
+.b(b),
+.sum(sum_reg)
 );
-
 // Instanciación del módulo uart_tx
 uart_tx uart_tx_inst (
-    .clk(clk),
-    .resetn(reset_n), // Se corrigió el nombre del puerto de entrada
-    .uart_txd(uart_txd),
-    .uart_tx_busy(uartbusy), // Se corrigió el nombre del puerto de salida
-    .uart_tx_en(uart_tx_en),
-    .uart_tx_data(sum_reg) // Se utiliza la suma como dato a enviar por UART
+.clk(clk),
+.resetn(reset_n), // Se corrigió el nombre del puerto de entrada
+.uart_txd(uart_txd),
+.uart_tx_busy(uartbusy), // Se corrigió el nombre del puerto de salida
+.uart_tx_en(uart_tx_en),
+.uart_tx_data(sum_reg) // Se utiliza la suma como dato a enviar por UART
 );
-
 endmodule
