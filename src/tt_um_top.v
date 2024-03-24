c8e3841fad093d0a1cba7b32fde28c5af28351c3
brtgio <67073086+brtgio@users.noreply.github.com>
2024-03-23 20:05:40 -0700

    Update tt_um_top.v

--- src/tt_um_top.v
+++ src/tt_um_top.v
@@ -15,20 +15,22 @@ module tt_um_top (
     input  wire       rst_n     // reset_n - low to reset
 );
 
-    fourbit_adder #(
+    latch_2x8 #(
     ) top_inst (
-     .a(ui_in[3:0]),          // Asignando el bit 7 de ui_in (clk) al puerto clk del módulo top_inst
-     .b(ui_in[7:4]),      // Asignando el bit 6 de ui_in (rst_n) al puerto reset_n del módulo top_inst
-     .sum(uo_out[4:0])
+     .clk(ui_in[0]),          // Asignando el bit 7 de ui_in (clk) al puerto clk del módulo top_inst
+     .reset_n(ui_in[1]),      // Asignando el bit 6 de ui_in (rst_n) al puerto reset_n del módulo top_inst
+     .save_a_n(ui_in[2]),
+     .save_a_n(ui_in[3]),
+     .data_input(ui_in[7:4]),
+     .q_a(uio_out[3:0]),
+     .q_b(uio_out[7:4])
     );
-    
     // Asignando los bits no usados a 0
- assign uo_out [7:5] = 3'b00;
+ assign uo_out [7:0] =  8'b11111111;
     
     
     // Configurando uio_oe como entrada (asignando todos los bits a 1)
-    assign uio_out [7:0] = 8'b11111111;
- assign uio_oe [7:0] = 8'b11111111;
+ assign uio_oe [7:0] = 8'b00000000;
 endmodule
 
 
