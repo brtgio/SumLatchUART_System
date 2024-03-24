585922d43160a15c341cd74dc3794bfecc3731bc
brtgio <67073086+brtgio@users.noreply.github.com>
2024-03-23 21:02:44 -0700

    Update tt_um_top.v

--- src/tt_um_top.v
+++ src/tt_um_top.v
@@ -15,19 +15,18 @@ module tt_um_top (
     input  wire       rst_n     // reset_n - low to reset
 );
 
-    latch_2x8  top_inst (
-     .clk(ui_in [0]),         
-     .reset_n(ui_in [1]),      
-     .save_a_n(ui_in [2]),
-     .save_b_n(ui_in [3]),
-     .data_input(ui_in [7:4]),
-     .q_a(uo_out [3:0]),
-     .q_b(uo_out [7:4])
+    toptop  top_inst (
+        .clk(ui_in [7]),         
+        .reset_n(ui_in [0]),      
+        .save_a_n(ui_in [1]),
+        .save_b_n(ui_in [2]),
+        .data_input(ui_in [6:3]),
+        .uart_txd(uio_out [0])
+        .uart_tx_busy(uo_out [7])
     );
     // Assigning the non-used bits to 0
-    assign uio_out [7:0] = 8'b00000000;
-    
-    
+    assign uo_out [6:0] = 7'b00000000;
+    assign uio_out [7:1] = 7'b00000000;
     // Configuring uio_oe as output (assigning all bits to 1)
     assign uio_oe [7:0] = 8'b11111111;
 endmodule
