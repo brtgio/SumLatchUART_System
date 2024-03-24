7d422e27b0e86f504bb6fbcedfbbd99b6fbd6ba9
brtgio <67073086+brtgio@users.noreply.github.com>
2024-03-23 21:34:30 -0700

    Update tt_um_top.v

--- src/tt_um_top.v
+++ src/tt_um_top.v
@@ -16,17 +16,18 @@ module tt_um_top (
 );
 
     toptop  top_inst (
-        .clk(clk),                // Connect top-level clock signal
-        .reset_n(rst_n),          // Connect top-level reset signal
-        .uart_tx_en(ena),         // Connect enable signal to UART transmitter
-        .uart_tx_data(ui_in[6:3]),// Connect data_input signal to UART transmitter
-        .uart_txd(uio_out[0]),    // UART transmit data connected to the least significant bit of uio_out
-        .uart_tx_busy(uio_out[7]) // UART transmit busy signal connected to the most significant bit of uio_out
+        .clk(ui_in[0]),                // Connect top-level clock signal
+        .reset_n(ui_in[1]),          // Connect top-level reset signal
+        .save_a_n(ui_in[2]),
+        .save_b_n(ui_in[3]),
+        .data_input(ui_in[7:4]),
+        .uart_tx_busy(uo_out[0]), // UART transmit busy signal connected to the most significant bit of uio_out
+        .uart_tx_busy(uo_out[1])
     );
 
     // Assigning the non-used output bits to 0
-    assign uo_out[7:0] = 8'b00000000;
-
+    assign uo_out [7:2] = 6'b000000;
+    assign uio_out [7:0] = 8'b00000000;
     // Configuring uio_oe as output (assigning all bits to 1)
     assign uio_oe[7:0] = 8'b11111111;
 
