f3d378ebf678f124fded73006036694ae87ee413
brtgio <67073086+brtgio@users.noreply.github.com>
2024-03-26 18:52:48 -0700

    Update top.v

--- src/top.v
+++ src/top.v
@@ -1,44 +1,49 @@
-module top(
-input clk,
-input reset_n,
-input save_a_n,
-input save_b_n,
-input [3:0] data_input,
-output uartbusy,
-input uart_tx_en, 
-output uart_txd
+module top (
+    input clk,
+    input reset_n,
+    input save_a_n,
+    input save_b_n,
+    input [3:0] ena,
+    input [3:0] data_input,
+	 input uart_tx_en,
+	 output  uartbusy,
+	 output uart_txd
+
 );
 
 // Declaración de registros internos para los datos de entrada de latch
 wire [3:0] a, b; 
+wire [7:0] data_send;
 
-// Declaración de registro interno para almacenar la suma
-wire [4:0] sum_reg; 
-wire [7:0] sum_reg_extended;
-assign sum_reg_extended = {3'b000, sum_reg};
-// Instanciación del módulo eight_bit_latch
+// Instancia de latch
 latch_2x8 latch_inst (
-.clk(clk),
-.reset_n(reset_n),
-.save_a_n(save_a_n),
-.save_b_n(save_b_n),
-.data_input(data_input),
-.q_a(a), // Salida del latch para el primer operando
-.q_b(b)  // Salida del latch para el segundo operando
+    .clk(clk),
+    .reset_n(!reset_n),
+    .save_a_n(save_a_n),
+    .save_b_n(save_b_n),
+    .data_input(data_input),
+    .q_a(a), // Salida del latch para el primer operando
+    .q_b(b)  // Salida del latch para el segundo operando
 );
-// Instanciación del módulo sum4bit_reg
-fourbit_adder sum_inst (
-.a(a),
-.b(b),
-.sum(sum_reg)
+
+// Instancia de la ALU
+alu alu_inst (
+    .clk(clk),
+	 .reset_n(!reset_n),
+    .ena(ena),
+    .a(a),
+    .b(b),
+    .result(data_send),
 );
-// Instanciación del módulo uart_tx
-uart_tx uart_tx_inst (
-.clk(clk),
-.reset_n(reset_n), // Se corrigió el nombre del puerto de entrada
-.uart_txd(uart_txd),
-.uartbusy(uartbusy), // Se corrigió el nombre del puerto de salida
-.uart_tx_en(uart_tx_en),
-.uart_tx_data(sum_reg_extended) // Se utiliza la suma como dato a enviar por UART
+
+// Instancia de UART
+uart_tx uart_inst (
+    .clk(clk),
+    .reset_n(reset_n),
+    .uart_tx_en(uart_tx_en), // Usando ena[0] para habilitar la transmisión UART
+    .uart_tx_data(data_send), // Transmitir el resultado a través de UART
+    .uart_txd(uart_txd), // Conectar a la línea de transmisión UART
+    .uartbusy(uartbusy) // Indicador de ocupado de UART
 );
+
 endmodule
